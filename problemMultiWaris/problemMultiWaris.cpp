0606ef8db22aa68c7a845cb0b1b298bf8a572dc1
alfinur154 <alfinur27@gmail.com>
2023-05-29 10:51:32 +0700

    membuat class budi beserta dengan contructor

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -26,9 +26,22 @@ public:
 
     pelajar(int pUmur) :
         orang(pUmur)
-    {
-        cout << "pelajar dibuat\n" << endl;
+         {
+               cout << "pelajar dibuat\n" << endl;
+
     }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+
 };
 
 
